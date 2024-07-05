331616a042066beb47be5cf59fc82a58148eba8a
phara.kit <pharada934@gmail.com>
2024-07-05 10:50:38 +0700

    first commit

---
+++ src/page/home/Home.jsx
@@ -0,0 +1,111 @@
+export default function Home() {
+    return (
+      <>
+        <header className="relative h-screen w-screen">
+        <img
+            className="absolute inset-0 object-cover h-full w-full"
+            src="https://i.pinimg.com/736x/31/7d/bf/317dbf6969a0c91379e23a5999f773b3.jpg"
+            alt="Background image description"
+        />
+
+        <div className="absolute inset-0 flex items-center justify-center pt-10">
+            <div className="w-full md:w-1/2 p-8">
+            <div className="flex flex-col md:flex-row justify-between items-center gap-10">
+                <img
+                className="h-auto w-full md:w-1/3 object-cover"
+                src="https://i0.wp.com/port2flavors.com/wp-content/uploads/2022/07/placeholder-614.png?fit=1200%2C800&ssl=1"
+                alt="First image description"
+                />
+                <img
+                className="h-auto w-full md:w-1/3 object-cover"
+                src="https://i0.wp.com/port2flavors.com/wp-content/uploads/2022/07/placeholder-614.png?fit=1200%2C800&ssl=1"
+                alt="Second image description"
+                />
+                <img
+                className="h-auto w-full md:w-1/3 object-cover"
+                src="https://i0.wp.com/port2flavors.com/wp-content/uploads/2022/07/placeholder-614.png?fit=1200%2C800&ssl=1"
+                alt="Third image description"
+                />
+            </div>
+            </div>
+        </div>
+    </header>
+        <section>
+            
+            <h1 className="text-5xl text-center font-extrabold dark:text-white pt-9 ">កិឡាដែលពេញនិយម 1</h1>
+            <h5 className="text-xl text-center font-bold dark:text-white pt-6">«យើង​មិន​ចង់​ប្រាប់​ពី​សុបិន​របស់​យើង​ទេ យើង​ចង់​បង្ហាញ​វា»។ Cristiano Ronaldo 
+            កីឡាករបាល់ទាត់ព័រទុយហ្គាល់ 5</h5>
+            
+        </section>
+        
+        <section className="relative flex items-center justify-center pt-10">
+        <div className="w-full md:w-1/2 p-8">
+            <div className="flex flex-col md:flex-row justify-between items-center gap-10">
+                <img
+                className="h-96 w-full md:w-1/3 object-cover"
+                src="https://i.pinimg.com/564x/a4/07/db/a407db00cc200b7c20f6136bd1d10680.jpg"
+                alt="First image description"
+                />
+                <img
+                className="h-96 w-full md:w-1/3 object-cover"
+                src="https://i.pinimg.com/564x/15/12/60/151260f98b497d438175419023c332fc.jpg"
+                alt="Second image description"
+                />
+                <img
+                className="h-96 w-full md:w-1/3 object-cover"
+                src="https://i.pinimg.com/736x/86/e5/04/86e5045baee10db0784804aa65119585.jpg"
+                alt="Third image description"
+                />
+            </div>
+            </div>
+        </section>
+        <section>
+            
+            <h1 className="text-5xl text-center font-extrabold dark:text-white pt-9 ">កម្មវិធីដែលយើងមាន</h1>
+            <h5 className="text-xl text-center font-bold dark:text-white pt-6">Sport Hub​ ជា Website និយាយពីកីឡាដែលពេញនិយមជាងគេ ហើយព័ត៏មាន​​ប្រកប
+            ​​                 ​​​              ទៅដោយទំនុកចិត្តសម្រាប់អ្នកស្រោលាញ់កីឡា។</h5>
+            
+        </section>
+        <section className="relative flex items-center justify-center pt-10">
+        <div className="w-full md:w-1/2 p-8">
+            <div className="flex flex-col md:flex-row justify-between items-center gap-10">
+
+            <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">🛌</h5>
+            <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">ព័ត៏មានថ្មីៗ</h1>
+            <p className="font-normal text-gray-700 dark:text-gray-400">Sport Hub​​ មានការ update ព័ត៏មានជារាងរាល់ថ្ងៃ។</p>
+            </a>
+
+            <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">🛌</h5>
+            <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">ព័ត៏មានថ្មីៗ</h1>
+            <p className="font-normal text-gray-700 dark:text-gray-400">Sport Hub​​ មានការ update ព័ត៏មានជារាងរាល់ថ្ងៃ។</p>
+            </a>
+
+            <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">🛌</h5>
+            <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">ព័ត៏មានថ្មីៗ</h1>
+            <p className="font-normal text-gray-700 dark:text-gray-400">Sport Hub​​ មានការ update ព័ត៏មានជារាងរាល់ថ្ងៃ។</p>
+            </a>
+
+        </div>
+        </div>   
+        
+        </section>
+
+        <section className="relative flex items-left justify-left ml-80 mt-10 mb-10">
+        <img className="h-auto max-w-lg" src="https://i.pinimg.com/736x/86/e5/04/86e5045baee10db0784804aa65119585.jpg" alt="image description"/>
+
+        <h1 className="mb-2 ml-96 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">ព័ត៏មានថ្មីៗ</h1>
+        
+        </section>
+        <section className="relative flex items-left justify-left ml-80 mt-10 mb-10">
+        <hr className="mb-2 border-b-2 border-gray-700 w-full" />
+        
+        </section>
+        
+        
+      </>
+    )
+  }
+  
